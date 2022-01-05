49f10c5f81533ed59252efd95546bd70ae502487
Joao Paulo Versiani Ladeia <joaopauloversianiladeia@gmail.com>
2022-01-05 13:36:34 -0300

    calculo de pi

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+import os
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app, resource={r"/*":{"origins":"*"}})
+
 @app.route("/", methods = ["GET","POST"])
 def index():
-    variavel = "Game: Adivinhe o game correto"
+    variavel = "Digite o numero de iteracoes para o calculo de PI:"
     if request.method == "GET":
         return render_template("index.html",variavel=variavel)
     else:
-        numero = 0
-        palpite = int(request.form.get("name"))
-        if numero == palpite:
-            return "<h1>Resultado: Voce ganhou!</h1>"
-        else:
-            return "<h1>Resultado: Voce perdeu...</h1>"    
+        i = int(request.form.get("name"))
+        k = 1
+        s = 0
+        for i in range(i): 
+            if i % 2 == 0: 
+                s += 4/k 
+            else: 
+                s -= 4/k
+            k += 2
+        return "<h1>O valor de PI calculado foi: "+str(s)+"</h1>"
 
 @app.route("/sobre")
 def sobre():
     return "<h1>Sobre</h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
